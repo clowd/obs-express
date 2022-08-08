f23e4813ef9006c050e70f37346acf6137f24a2d
Caelan Sayler <git@caesay.com>
2022-08-08 12:54:41 +0100

    Remove extra locales and rename artifact

--- pack-release.ps1
+++ pack-release.ps1
@@ -10,6 +10,17 @@ msbuild ObsExpressCpp.sln -p:Configuration=Release -p:Platform=x64
 $ReleaseDir = Resolve-Path -Path "build64/rundir/MinSizeRel"
 $BinDir = Resolve-Path -Path "$ReleaseDir/bin/64bit"
 
+# remove non en-US locale's
+$localeFiles = Get-ChildItem -Path $ReleaseDir -Filter *.ini -Recurse -ErrorAction SilentlyContinue -Force
+foreach($file in $localeFiles)
+{
+    if ($file.FullName -notmatch 'en-US.ini$') {
+        Remove-Item -Path $file.FullName -ErrorAction Ignore
+    }
+}
+
+
+# create final zip
 Remove-Item -Path "$BinDir/*.pdb" -ErrorAction Ignore
-Remove-Item -Path "release.zip" -ErrorAction Ignore
-seven a release.zip -y -mx9 `"${ReleaseDir}/*`"
+Remove-Item -Path "obs-express.zip" -ErrorAction Ignore
+seven a obs-express.zip -y -mx9 `"${ReleaseDir}/*`"
