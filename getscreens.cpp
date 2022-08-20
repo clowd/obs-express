8b27f077a2df82d17d3c6c74f16f4acd76aab4c4
Caelan Sayler <git@caesay.com>
2022-08-20 12:15:21 +0100

    Support retrieval of monitor friendly name

--- getscreens.cpp
+++ getscreens.cpp
@@ -4,6 +4,53 @@
 
 using namespace std;
 
+static bool GetMonitorTarget(LPCWSTR device, DISPLAYCONFIG_TARGET_DEVICE_NAME* target)
+{
+    bool found = false;
+    UINT32 numPath, numMode;
+    if (GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &numPath, &numMode) == ERROR_SUCCESS) {
+        DISPLAYCONFIG_PATH_INFO* paths = (DISPLAYCONFIG_PATH_INFO*)malloc(numPath * sizeof(DISPLAYCONFIG_PATH_INFO));
+        DISPLAYCONFIG_MODE_INFO* modes = (DISPLAYCONFIG_MODE_INFO*)malloc(numMode * sizeof(DISPLAYCONFIG_MODE_INFO));
+        if (QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &numPath, paths, &numMode, modes, NULL) == ERROR_SUCCESS) {
+            for (size_t i = 0; i < numPath; ++i) {
+                const DISPLAYCONFIG_PATH_INFO* const path = &paths[i];
+                DISPLAYCONFIG_SOURCE_DEVICE_NAME source;
+                source.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_SOURCE_NAME;
+                source.header.size = sizeof(source);
+                source.header.adapterId = path->sourceInfo.adapterId;
+                source.header.id = path->sourceInfo.id;
+                if (DisplayConfigGetDeviceInfo(&source.header) == ERROR_SUCCESS && wcscmp(device, source.viewGdiDeviceName) == 0) {
+                    target->header.type = DISPLAYCONFIG_DEVICE_INFO_GET_TARGET_NAME;
+                    target->header.size = sizeof(*target);
+                    target->header.adapterId = path->sourceInfo.adapterId;
+                    target->header.id = path->targetInfo.id;
+                    found = DisplayConfigGetDeviceInfo(&target->header) == ERROR_SUCCESS;
+                    break;
+                }
+            }
+        }
+
+        free(modes);
+        free(paths);
+    }
+
+    return found;
+}
+
+static void GetMonitorName(HMONITOR handle, char* name, size_t count)
+{
+    MONITORINFOEXW mi;
+    DISPLAYCONFIG_TARGET_DEVICE_NAME target;
+
+    mi.cbSize = sizeof(mi);
+    if (GetMonitorInfoW(handle, (LPMONITORINFO)&mi) && GetMonitorTarget(mi.szDevice, &target)) {
+        snprintf(name, count, "%ls", target.monitorFriendlyDeviceName);
+    }
+    else {
+        strcpy_s(name, count, "[OBS: Unknown]");
+    }
+}
+
 BOOL __cdecl EnumMonitorCallback(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
 {
     std::vector<screen_info>& infos = *((std::vector<screen_info>*)dwData);
@@ -21,6 +68,9 @@ BOOL __cdecl EnumMonitorCallback(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcM
     device.cb = sizeof(device);
     EnumDisplayDevicesA(mon_info.szDevice, 0, &device, EDD_GET_DEVICE_INTERFACE_NAME);
 
+    char monitor_name[64];
+    GetMonitorName(hMonitor, monitor_name, sizeof(monitor_name));
+
     infos.emplace_back(
         mon_info.rcMonitor.left,
         mon_info.rcMonitor.top,
@@ -28,7 +78,8 @@ BOOL __cdecl EnumMonitorCallback(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcM
         mon_info.rcMonitor.bottom - mon_info.rcMonitor.top,
         dpiX,
         device.DeviceID,
-        mon_info.szDevice);
+        mon_info.szDevice,
+        monitor_name);
 
     return TRUE;
 }
--- getscreens.h
+++ getscreens.h
@@ -4,11 +4,12 @@
 
 struct screen_info
 {
-    screen_info(int32_t x, int32_t y, int32_t width, int32_t height, uint32_t dpi, char id[128], char name[64])
-        : x(x), y(y), width(width), height(height), dpi(dpi), monitor_id(""), monitor_name("")
+    screen_info(int32_t x, int32_t y, int32_t width, int32_t height, uint32_t dpi, char id[128], char name[64], char friendly_name[64])
+        : x(x), y(y), width(width), height(height), dpi(dpi), monitor_id(""), monitor_device_name(""), monitor_friendly_name("")
     {
         strcpy_s(monitor_id, _countof(monitor_id), id);
-        strcpy_s(monitor_name, _countof(monitor_name), name);
+        strcpy_s(monitor_device_name, _countof(monitor_device_name), name);
+        strcpy_s(monitor_friendly_name, _countof(monitor_friendly_name), friendly_name);
     }
     int32_t x;
     int32_t y;
@@ -16,7 +17,8 @@ struct screen_info
     int32_t height;
     uint32_t dpi;
     char monitor_id[128];
-    char monitor_name[64];
+    char monitor_device_name[64];
+    char monitor_friendly_name[64];
 };
 
 struct mouse_info
