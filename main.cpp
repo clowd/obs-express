b2d70d4d0b944678ae23c5c48e2c8f9f0ce7d9e4
Caelan Sayler <git@caesay.com>
2022-09-10 14:49:28 +0100

    bump version to v0.0.7

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@
 #pragma comment(lib, "gdiplus.lib")
 #pragma comment(lib, "obs.lib")
 
-#define OBS_EXPRESS_VERSION "0.0.6"
+#define OBS_EXPRESS_VERSION "0.0.7"
 
 using namespace std;
 using namespace Gdiplus;
